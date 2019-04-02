b6f249f715b256ec6e520c68f6648210fe56fa97
igor <igor.soldatov@gmail.com>
2019-04-02 10:52:16 +0300

    New asset price operations #0

--- libraries/protocol/new_asset.cpp
+++ libraries/protocol/new_asset.cpp
@@ -289,6 +289,95 @@ asset_symbol_type::asset_symbol_space asset_symbol_type::space()const
    return s;
 }
 
+void asset_symbol_type::validate()const
+{
+   switch( asset_num )
+   {
+      case STEEM_ASSET_NUM_STEEM:
+      case STEEM_ASSET_NUM_SBD:
+      case STEEM_ASSET_NUM_VESTS:
+         break;
+      default:
+      {
+         uint32_t nai_data_digits = (asset_num >> 5);
+         uint32_t nai_1bit = (asset_num & 0x10);
+         uint32_t nai_decimal_places = (asset_num & 0x0F);
+         FC_ASSERT( (nai_data_digits >= SMT_MIN_NAI) &
+                    (nai_data_digits <= SMT_MAX_NAI) &
+                    (nai_1bit == 0x10) &
+                    (nai_decimal_places <= STEEM_ASSET_MAX_DECIMALS),
+                    "Cannot determine space for asset ${n}", ("n", asset_num) );
+      }
+   }
+   // this assert is duplicated by above code in all cases
+   // FC_ASSERT( decimals() <= STEEM_ASSET_MAX_DECIMALS );
+}
+
+void asset::validate()const
+{
+   symbol.validate();
+   FC_ASSERT( amount.value >= 0 );
+   FC_ASSERT( amount.value <= STEEM_MAX_SATOSHIS );
+}
+
+#define BQ(a) \
+   std::tie( a.base.symbol, a.quote.symbol )
+
+#define DEFINE_PRICE_COMPARISON_OPERATOR( op ) \
+bool operator op ( const price& a, const price& b ) \
+{ \
+   if( BQ(a) != BQ(b) ) \
+      return BQ(a) op BQ(b); \
+   \
+   const uint128_t amult = uint128_t( b.quote.amount.value ) * a.base.amount.value; \
+   const uint128_t bmult = uint128_t( a.quote.amount.value ) * b.base.amount.value; \
+   \
+   return amult op bmult;  \
+}
+
+DEFINE_PRICE_COMPARISON_OPERATOR( == )
+DEFINE_PRICE_COMPARISON_OPERATOR( != )
+DEFINE_PRICE_COMPARISON_OPERATOR( <  )
+DEFINE_PRICE_COMPARISON_OPERATOR( <= )
+DEFINE_PRICE_COMPARISON_OPERATOR( >  )
+DEFINE_PRICE_COMPARISON_OPERATOR( >= )
+
+      asset operator * ( const asset& a, const price& b )
+      {
+         if( a.symbol == b.base.symbol )
+         {
+            FC_ASSERT( b.base.amount.value > 0 );
+            uint128_t result = (uint128_t(a.amount.value) * b.quote.amount.value)/b.base.amount.value;
+            FC_ASSERT( result.hi == 0 );
+            return asset( result.to_uint64(), b.quote.symbol );
+         }
+         else if( a.symbol == b.quote.symbol )
+         {
+            FC_ASSERT( b.quote.amount.value > 0 );
+            uint128_t result = (uint128_t(a.amount.value) * b.base.amount.value)/b.quote.amount.value;
+            FC_ASSERT( result.hi == 0 );
+            return asset( result.to_uint64(), b.base.symbol );
+         }
+         FC_THROW_EXCEPTION( fc::assert_exception, "invalid asset * price", ("asset",a)("price",b) );
+      }
+
+      price operator / ( const asset& base, const asset& quote )
+      { try {
+         FC_ASSERT( base.symbol != quote.symbol );
+         return price{ base, quote };
+      } FC_CAPTURE_AND_RETHROW( (base)(quote) ) }
+
+      price price::max( asset_symbol_type base, asset_symbol_type quote ) { return asset( share_type(STEEM_MAX_SATOSHIS), base ) / asset( share_type(1), quote); }
+      price price::min( asset_symbol_type base, asset_symbol_type quote ) { return asset( 1, base ) / asset( STEEM_MAX_SATOSHIS, quote); }
+
+      bool price::is_null() const { return *this == price(); }
+
+      void price::validate() const
+      { try {
+         FC_ASSERT( base.amount > share_type(0) );
+         FC_ASSERT( quote.amount > share_type(0) );
+         FC_ASSERT( base.symbol != quote.symbol );
+      } FC_CAPTURE_AND_RETHROW( (base)(quote) ) }
 
 } } // bmchain::protocol
 
